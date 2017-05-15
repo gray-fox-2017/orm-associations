dabfc5386134a1dda287b740b68e063e231fc8c2
AndrewSenewe <sindrew.christ@gmail.com>
2017-05-15 08:25:52 +0700

    DOne

---
+++ seeders/20170515004117-student.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    return queryInterface.bulkInsert ('Students', [{
+      first_name: 'Andrew',
+      last_name: 'Senewe',
+      birthdate: new Date('1994-03-31'),
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {})
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Students', null, {})
+  }
+};
---
+++ seeders/20170515004533-teacher.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Teachers', [{
+      name: 'Ells Schön',
+      email: 'ells@gmail.com',
+      phone: '081234566543',
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {})
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Teachers', null, {})
+  }
+};
